2546bb4ea214b87cac1a6d21fd37764566a82c94
Samuel Slivovsky <scool366@gmail.com>
2022-10-29 20:09:37 +0200

    better version of calendar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import "./App.css";
 import Menu from "./TabMenu/Menu";
-import Calendar from './Calendar/Calendar'
+import Calendar from "./Calendar/Calendar";
 function App() {
   return (
-  <div>
-  <Menu />
-
-<div className="kalendar" style={{width:'50%', height:'50%' }}>
-  <Calendar></Calendar>
-  </div>
-  </div>
+    <div>
+      <Menu />
+      <div className="kalendar-obal">
+        <div className="kalendar">
+          <Calendar></Calendar>
+        </div>
+      </div>
+    </div>
   );
 }
 export default App;
--- src/Calendar/Calendar.jsx
+++ src/Calendar/Calendar.jsx
@@ -1,40 +1,72 @@
-import FullCalendar from "@fullcalendar/react";
+import React from "react";
+import FullCalendar, { formatDate } from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
-// import timeGridPlugin from "@fullcalendar/timegrid";
-
-//import "@fullcalendar/core/main.css";
-import "@fullcalendar/daygrid/main.css";
-import "@fullcalendar/timegrid/main.css";
-
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
 import events from "./events";
 
-export default function App() {
+export default class DemoApp extends React.Component {
+  render() {
+    return (
+      <div className="demo-app">
+        <div className="demo-app-main">
+          <FullCalendar
+            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+            headerToolbar={{
+              left: "prev,next today",
+              center: "title",
+              right: "dayGridMonth,timeGridWeek,timeGridDay",
+            }}
+            initialView="dayGridMonth"
+            editable={true}
+            selectable={true}
+            events={events}
+            select={this.handleDateSelect}
+            eventClick={this.handleEventClick}
+            eventsSet={this.handleEvents} // called after events are initialized/added/changed/removed
+            /* you can update a remote database when these fire:
+            eventAdd={function(){}}
+            eventChange={function(){}}
+            eventRemove={function(){}}
+            */
+          />
+        </div>
+      </div>
+    );
+  }
+
+  handleWeekendsToggle = () => {
+    this.setState({
+      weekendsVisible: !this.state.weekendsVisible,
+    });
+  };
+
+  handleDateSelect = (selectInfo) => {
+    let title = prompt("Please enter a new title for your event");
+  };
+
+  handleEventClick = (clickInfo) => {
+    if (
+      window.confirm(
+        `Are you sure you want to delete the event '${clickInfo.event.title}'`
+      )
+    ) {
+      clickInfo.event.remove();
+    }
+  };
+
+  handleEvents = (events) => {
+    this.setState({
+      currentEvents: events,
+    });
+  };
+}
+
+function renderEventContent(eventInfo) {
   return (
-    <div className="App">
-      <FullCalendar
-        defaultView="dayGridMonth"
-        header={{
-          left: "prev,next",
-          center: "title",
-          right: "dayGridMonth,timeGridWeek,timeGridDay"
-        }}
-        themeSystem="Simplex"
-        plugins={[dayGridPlugin]}
-        events={events}
-      />
-      <FullCalendar
-        defaultView="dayGridMonth"
-        // themeSystem="Simplex"
-        // header={{
-        //   left: "prev,next",
-        //   center: "title",
-        //   right: "dayGridMonth,timeGridWeek,timeGridDay",
-        // }}
-        plugins={[dayGridPlugin]}
-        events={events}
-        displayEventEnd="true"
-        eventColor={"#" + Math.floor(Math.random() * 16777215).toString(16)}
-      />
-    </div>
+    <>
+      <b>{eventInfo.timeText}</b>
+      <i>{eventInfo.event.title}</i>
+    </>
   );
 }
